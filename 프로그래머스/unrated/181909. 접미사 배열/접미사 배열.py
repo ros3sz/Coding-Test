e21aafe4a45d4908a6e7a4f6c9986fb344a84def
Sujin H <139188632+ros3sz@users.noreply.github.com>
2023-09-21 09:27:58 +0900

    [unrated] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.py
@@ -1,4 +1,5 @@
 def solution(my_string):
-    answer = [my_string[-i:] for i in range(len(my_string))]
-    answer.sort()
+    answer = sorted([my_string[-i:] for i in range(len(my_string))])
+    # sort : 원본을 정렬하고 수정합니다 (in-place)
+    # sorted : 원본은 유지하고 정렬한 새 리스트를 만듭니다.
     return answer
